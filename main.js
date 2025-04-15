f5df6373bbeef2cef6b8c8e5edff4dc6c983c303
Kouzin1 <samuelakinwunmi2@gmail.com>
2025-04-15 22:58:49 +0100

    Update

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ let lastChecked;
 function handleCheck(e) {
   let inBetween = false;
   if (e.shiftKey && this.checked) {
+    checkboxes.forEach((checkbox) => {
+      console.log(checkbox);
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+        console.log("Starting to check");
+      }
+    });
   }
 
   lastChecked = this;
