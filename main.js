fe4dee432d2d337cb5cfcc2768879d1165c48942
Kouzin1 <samuelakinwunmi2@gmail.com>
2025-04-15 23:04:02 +0100

    Comment

--- main.js
+++ main.js
@@ -3,13 +3,20 @@ const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 let lastChecked;
 
 function handleCheck(e) {
+  // check if they had the shift keydown
+  // AND check that they are checking it
   let inBetween = false;
   if (e.shiftKey && this.checked) {
+    // go ahead and do what we please
+    // loop over every single checkbox
     checkboxes.forEach((checkbox) => {
       console.log(checkbox);
       if (checkbox === this || checkbox === lastChecked) {
         inBetween = !inBetween;
         console.log("Starting to check");
+      }
+      if (inBetween) {
+        checkbox.checked = true;
       }
     });
   }
